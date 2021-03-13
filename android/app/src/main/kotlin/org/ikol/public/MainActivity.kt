cf117aee8d6ed269ab0a3ce1e8cc6db76f8f5b58
phanirithvij <phanirithvij2000@gmail.com>
2021-03-13 15:16:52 +0530

    remove previous stuff

--- android/app/src/main/kotlin/org/ikol/public/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.ikol.public
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/org/ikol/public_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.ikol.public_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
