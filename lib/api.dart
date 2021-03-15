fc6e77012d93ddffa012957114775dab48ff517c
phanirithvij <phanirithvij2000@gmail.com>
2021-03-15 16:06:24 +0530

    home page

--- lib/api.dart
+++ lib/api.dart
@@ -1,18 +1,37 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:public_app/http/code.dart';
 
 const String BaseURL = 'http://192.168.0.101:3000';
 
 class APIMethods {
-  static Future<void> toggleActive(bool active) async {
-    const url = '/public_users/user/604a724263d642654fd8b333/actions/active';
-    await http.post(
-      Uri.parse("$BaseURL/$url"),
+  static Future<List> toggleActive(bool active) async {
+    const route = '/public_users/user/604a724263d642654fd8b333/actions/active';
+    var resp = await http.put(
+      Uri.parse("$BaseURL$route"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: json.encode({'active': active}),
+      body: jsonEncode({'active': active}),
     );
+    // TODO https://github.com/jogboms/flutter_offline
+    // TODO https://medium.com/flutter-community/build-a-network-sensitive-ui-in-flutter-using-provider-and-connectivity-ddad140c9ff8
+    var data = jsonDecode(resp.body);
+    print(data);
+    switch (resp.statusCode) {
+      case HttpStatus.badRequest:
+        return [false, "Bad request"];
+        break;
+      case HttpStatus.ok:
+        return [true, "", data['details']['active'] as bool];
+        break;
+      case HttpStatus.notFound:
+        return [false, "User not Found"];
+        break;
+      default:
+        // internal server error
+        return [false, "An error occurred on the server"];
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:public_app/api.dart';
 import 'package:public_app/colors/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+const double _kMinCircularProgressIndicatorSize = 36.0;
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,7 +22,7 @@ class MyApp extends StatelessWidget {
         accentColor: kAccentColor,
         appBarTheme: AppBarTheme(backgroundColor: Colors.white),
       ),
-      home: MyHomePage(title: 'IKOL?'),
+      home: MyHomePage(title: 'IKOL'),
     );
   }
 }
@@ -31,21 +36,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _started = false;
-
-  Future<void> toggleActive() async {
-    await APIMethods.toggleActive(!_started);
-    setState(() {
-      _started = !_started;
-    });
-  }
+  // ...
+  // methods
 
   @override
   Widget build(BuildContext context) {
     var textStyle = TextStyle(
       color: Color.fromARGB(255, 91, 175, 110),
     );
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -69,46 +67,147 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
+      body: HomeWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MapsLauncher.launchCoordinates(37.4220041, -122.0862462);
+        },
+        backgroundColor: Colors.white,
+        child: Transform.translate(
+          offset: Offset(0, 4),
+          child: SvgPicture.asset(
+            "assets/gmaps.svg",
+            semanticsLabel: 'Open in Maps',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  HomeWidget({Key key}) : super(key: key);
+
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  var _active = false;
+  String _error;
+  var _loading = false;
+
+  // TODO ui click threshold
+  Timer _debounce;
+
+  void showSnackBar(String value) {
+    // https://stackoverflow.com/a/49932085/8608146
+    Scaffold.of(context)
+        // ignore: deprecated_member_use
+        .showSnackBar(
+      SnackBar(
+        content: Text(value),
+        action: SnackBarAction(
+          label: "DISMISS",
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  void toggleActive() {
+    if (_loading) return;
+    // https://stackoverflow.com/a/52930197/8608146
+    if (_debounce?.isActive ?? false) _debounce.cancel();
+
+    // if loading we anyway return
+    // so this will only trigger build when needed
+    setState(() {
+      _loading = true;
+    });
+    _debounce = Timer(const Duration(milliseconds: 400), () async {
+      var _x = await APIMethods.toggleActive(!_active);
+      final bool success = _x[0];
+      final String error = _x[1];
+      setState(() {
+        if (success) {
+          final bool active = _x[2];
+          _active = active;
+          // remove any previous error
+          _error = null;
+        } else {
+          _error = error;
+        }
+        // we're not loading anymore
+        _loading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_error != null) {
+      // show snackbar
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        showSnackBar(_error);
+      });
+    }
+
+    return Container(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ClipOval(
-              child: Material(
-                color: _started
-                    ? Color.fromARGB(255, 244, 47, 35)
-                    : Color.fromARGB(255, 82, 167, 81),
-                child: InkWell(
-                  onTap: () {
-                    toggleActive();
-                  },
-                  splashColor: _started
-                      ? Color.fromARGB(255, 136, 48, 42)
-                      : kAccentColor,
-                  child: SizedBox(
-                    width: 220,
-                    height: 220,
-                    child: Center(
-                      child: Text(
-                        _started ? "STOP SHARING" : "SHARE LOCATION",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+            Center(
+              child: ClipOval(
+                child: Material(
+                  color: _active
+                      ? Color.fromARGB(255, 244, 47, 35)
+                      : Color.fromARGB(255, 82, 167, 81),
+                  child: InkWell(
+                    onTap: () {
+                      toggleActive();
+                    },
+                    splashColor: _active
+                        ? Color.fromARGB(255, 136, 48, 42)
+                        : kAccentColor,
+                    child: SizedBox(
+                      width: 220,
+                      height: 220,
+                      child: Center(
+                        child: Text(
+                          _active ? "STOP SHARING" : "SHARE LOCATION",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30),
+            ),
+            _loading
+                ? CircularProgressIndicator(
+                    semanticsLabel:
+                        "Turning ${_active ? "off" : "on"} location sharing",
+                  )
+                : Container(
+                    height: _kMinCircularProgressIndicatorSize,
+                  ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-        tooltip: "Add",
       ),
     );
   }
